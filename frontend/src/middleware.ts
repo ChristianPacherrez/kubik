ceecdea6f898f60c044ab674b62805d154b64abc
Christian Pacherrez <christian.j.pacherrez@gmail.com>
2026-05-22 00:51:00 -0500

    test: disable middleware to fix Vercel crash

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,22 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-])
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return
-
-  const { userId } = auth()
-
-  if (!userId) {
-    const signInUrl = new URL('/sign-in', req.url)
-    return Response.redirect(signInUrl)
-  }
-})
+export default function middleware() {
+  return
+}
 
 export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)'],
+  matcher: [],
 }
